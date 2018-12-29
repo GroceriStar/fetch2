d4c3228e5754ed6a3763c405951f46e630b5c3a1
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-12-29 12:58:09 +0200

    moving code from another repo

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import TodaysDate     from '~/TodaysDate'
+import List           from '~/List/List'
+import DefaultList    from '~/List/DefaultList'
+import ListF           from '~/List/ListF'
+
+import IngredientList from '~/IngredientList/IngredientList'
+import DepartmentList from '~/DepartmentList/DepartmentList'
+import GroceryList    from '~/GroceryList/GroceryList'
+
+export {
+  TodaysDate,
+  List,
+  DefaultList,
+  ListF,
+  
+  IngredientList,
+  DepartmentList,
+  GroceryList
+};

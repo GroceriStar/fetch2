b2ef50b1c1fa1f9ad9da5257b7ca49c6b54ba058
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-12-29 13:17:05 +0200

    first function that was build from fetch plugin

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 // import TodaysDate     from '~/TodaysDate'
 // import List           from '~/List/List'
 
+import { search } from '@groceristar/groceristar-fetch';
+
+
+const getData = (value, type = 'attribute') => {
+  let result
+  if( type == 'attribute' ){
+    result = search.getAttribute(value);
+  }
+
+  if( type == 'ingredient' ){
+    result = search.getIngredients();
+  }
+
+  return result;
+
+}
+
 
 export {
   // TodaysDate,
   // List,
-
+  getData
 };
